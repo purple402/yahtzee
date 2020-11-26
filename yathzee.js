943550c4e4bc8fe306b6bd821e38a6fe8f37e7f9
purple402 <purple402@naver.com>
2020-11-26 23:36:37 +0900

    make handleInput for save dice number

--- yathzee.js
+++ yathzee.js
@@ -1,8 +1,12 @@
 const dice = document.querySelectorAll('input');
+let diceValue = {};
 
 
 function handleInput(id, value) {
+    var diceNumber = id.charAt(id.length - 1);
 
+    diceValue[`${diceNumber}`] = value;
+    makeScore();
 }
 
 dice.forEach(input => {
